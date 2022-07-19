069c5367ac6840ca1af4e7e149d1d4771ea687a3
Arash <arashdarabi9574@gmail.com>
2022-07-19 19:10:29 +0430

    final version of Rainbow, add def function to it so its ready for use at any where in the code/program

--- Rainbow.py
+++ Rainbow.py
@@ -1,21 +1,22 @@
 import hashlib
 import csv
 from collections import OrderedDict
-with open('/home/adn/Documents/git-Cource/a.csv') as reader:
-    reader = csv.reader(reader)
-    mydict = OrderedDict()
-    dict_res = OrderedDict()
-    for i in range(9999):
-        hashlib_result = hashlib.sha256(str(i).encode())
-        mydict [hashlib_result.hexdigest()] = i
-    for j in reader:
-        our_name = j[0]
-        our_hashlib = j[1]
-        short_cut = mydict.keys()
-        for key in short_cut:
-            if our_hashlib == key:
-                dict_res[our_name] = mydict.get(key)
-with open('/home/adn/Documents/git-cource/1.csv', 'w') as result:
+def hash_password_hack(input_file_name, output_file_name):
+    with open(input_file_name) as reader:
+        reader = csv.reader(reader)
+        mydict = OrderedDict()
+        dict_res = OrderedDict()
+        for i in range(9999):
+            hashlib_result = hashlib.sha256(str(i).encode())
+            mydict [hashlib_result.hexdigest()] = i
+        for j in reader:
+            our_name = j[0]
+            lpass = j[1]
+            short_cut = mydict.keys()
+            for key in short_cut:
+                if lpass == key:
+                    dict_res[our_name] = mydict.get(key)
+    with open(output_file_name, 'w') as result:
         this_counter = 0
         for i in dict_res:
             this_counter += 1
